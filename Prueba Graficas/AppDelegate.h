c310d40d8430c5c3bd617d1ace37ae44b7e42c9c
Pedro Contreras <fpedro23@gmail.com>
2014-10-02 21:24:30 -0500

    Initial Commit

---
+++ Prueba Graficas/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Prueba Graficas
+//
+//  Created by Pedro Contreras Nava on 02/10/14.
+//  Copyright (c) 2014 Pedro Contreras Nava. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Prueba Graficas/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Prueba Graficas
+//
+//  Created by Pedro Contreras Nava on 02/10/14.
+//  Copyright (c) 2014 Pedro Contreras Nava. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
